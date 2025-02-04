25863eac6053fe243c357f111c8507d865c863d2
한하영 <hanhy0219@naver.com>
2025-02-04 15:56:35 +0900

    로그인 1차

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+
+const URL = "http://192.168.1.5:8080/api"; // 실제 백엔드 IP 주소
+// const URL = "http://localhost:8080/api";
+
+
 function Login() {
-  // 입력 값을 저장할 상태
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
 
-  // 입력 변경
+  
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -16,21 +21,22 @@ function Login() {
     });
   };
 
-  // 로그인 요청
+  
   const handleSubmit = async (e) => {
-    e.preventDefault(); // 새로고침X
+    e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost/api/user/login", formData);
+      
+      const response = await axios.post(`${URL}/user/login`, formData);
 
       console.log("로그인 성공:", response.data);
-      alert("로그인 성공!");
+      alert("로그인에 성공하였습니다.");
 
-      // 로그인 성공 후 토큰 저장
+      
       localStorage.setItem("token", response.data.token);
     } catch (error) {
       console.error("로그인 실패:", error.response?.data || error.message);
-      alert("로그인 실패! 이메일과 비밀번호를 확인하세요.");
+      alert("로그인에 실패하였습니다. 이메일과 비밀번호를 확인하세요.");
     }
   };
 
